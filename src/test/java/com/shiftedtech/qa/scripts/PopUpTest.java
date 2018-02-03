1d9330473b455a74d7f2351fa5f3107aa9b8b249
islam.fahd <teacher@shift.com>
2018-02-03 12:09:14 -0500

    popup test2

--- src/test/java/com/shiftedtech/qa/scripts/PopUpTest.java
+++ src/test/java/com/shiftedtech/qa/scripts/PopUpTest.java
@@ -1,12 +1,15 @@
 package com.shiftedtech.qa.scripts;
 
 import com.shiftedtech.qa.common.BaseClass;
+import com.shiftedtech.qa.utils.ElementUtils;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class PopUpTest extends BaseClass{
 
@@ -61,6 +64,52 @@ public class PopUpTest extends BaseClass{
 
     }
 
+    @Test
+    public void test3(){
+
+        driver.navigate().to("https://www.barnesandnoble.com/b/books/_/N-29Z8q8");
+        ElementUtils.delayFor(5000);;
+
+        WebElement signinLink = driver.findElement(By.id("signInLink"));
+        signinLink.click();
+
+        switchToIfrmae("loginFrame");
+        WebElement createAccount = driver.findElement(By.xpath(".//*[@id='createAccountBtn']"));
+        createAccount.click();
+
+    }
+
+    public void switchToIfrmae(String str) {
+        if (str.contentEquals("loginFrame")) {
+            List<WebElement> iframeList = driver.findElements(By.tagName("iframe"));
+            System.out.println("Number of Iframe: " + iframeList.size());
+
+            for (WebElement ifamre : iframeList) {
+                String src = ifamre.getAttribute("src");
+                System.out.println("Src" + src);
+                if (src.contains("/www.barnesandnoble.com/account/login-frame-ra")) {
+                    System.out.println("Iframe Found");
+                    driver.switchTo().frame(ifamre);
+                    break;
+                }
+            }
+        }
+
+        if (str.contentEquals("registerFrame")){
+            List<WebElement> iframeList = driver.findElements(By.tagName("iframe"));
+            System.out.println("Number of Iframe: " + iframeList.size());
+
+            for(WebElement ifamre : iframeList){
+                String src = ifamre.getAttribute("src");
+                System.out.println("Src" + src);
+                if(src.contains("/www.barnesandnoble.com/account/register-frame-ra")){
+                    System.out.println("Iframe Found");
+                    driver.switchTo().frame(ifamre);
+                    break;
+                }
+            }
+        }
+    }
 
 
 
